6f6f8e539d9e29f5eb8ddfd7818abccb3d34fe63
karolbgm <karolyne.morgan@outlook.com>
2024-02-03 10:45:23 -0700

    create, update, delete

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Bird
 # Add the following import
 from django.http import HttpResponse
 
+#FUNCTION BASED VIEWS
 # Define the home view
 def home(request):
   return render(request, 'home.html')
@@ -10,8 +12,33 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+def birds_index(request):
+  birds = Bird.objects.all()
+  return render(request, 'birds/index.html', { 'birds': birds })
 
-# Add the Bird class & list and view function below the imports
+def birds_detail(request, bird_id):
+  bird = Bird.objects.get(id=bird_id)
+  return render(request, 'birds/detail.html', { 'bird': bird })
+
+#CLASS BASED VIEWS
+class BirdCreate(CreateView):
+  model = Bird
+  fields = '__all__' #give me all the fields
+# success_url = '/birds/'
+
+class BirdUpdate(UpdateView):
+  model = Bird
+  fields = ['species', 'age']
+
+class BirdDelete(DeleteView):
+  model = Bird
+  success_url = '/birds/'
+
+
+
+
+
+  # Add the Bird class & list and view function below the imports
 # class Bird:
 #   def __init__(self, name, species, color, age):
 #     self.name = name
@@ -26,15 +53,5 @@ def about(request):
 # ]
 
 
-
-
 # def birds_index(request):
 #   return render(request, 'birds/index.html', {'birds': birds})
-
-def birds_index(request):
-  birds = Bird.objects.all()
-  return render(request, 'birds/index.html', { 'birds': birds })
-
-def birds_detail(request, bird_id):
-  bird = Bird.objects.get(id=bird_id)
-  return render(request, 'birds/detail.html', { 'bird': bird })
