c07a526c426e15b60578167d7bfc4192f8700e0c
karolbgm <karolyne.morgan@outlook.com>
2024-02-03 13:55:34 -0700

    admin page

--- finchcollector/main_app/admin.py
+++ finchcollector/main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Bird, Feeding
 
 # Register your models here.
+admin.site.register(Bird)
+# register the new Feeding Model
+admin.site.register(Feeding)
--- finchcollector/main_app/models.py
+++ finchcollector/main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 class Bird(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
@@ -11,3 +17,19 @@ class Bird(models.Model):
         return self.name
     def get_absolute_url(self):
         return reverse('detail', kwargs={'bird_id': self.id})
+    
+
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+      max_length=1,
+      choices=MEALS,
+      default=MEALS[0][0]
+      )
+  # Create a bird_id FK
+  bird = models.ForeignKey(Bird, on_delete=models.CASCADE)
+  #(if the bird is deleted, every feeding will be deleted)
+  
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
